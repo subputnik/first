b8940c52ad5df479bec8c8e279cbde70a29c365f
Dmitry <sub.putnik@gmail.com>
2024-08-31 15:05:58 +0300

    Add host to config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000
+  },
 })
